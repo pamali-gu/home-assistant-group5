be66618e24752816ddc3707fa2758d125b881b07
Marble879 <younisakel879@gmail.com>
2024-12-11 09:44:18 +0100

    Move sensor max and min retrieval to general helpers

--- homeassistant/components/lightmap/helpers.py
+++ homeassistant/components/lightmap/helpers.py
@@ -13,3 +13,25 @@ def get_sensors(hass: HomeAssistant) -> list:
             or state.attributes.get("unit_of_measurement") == "lux"
         )
     ]
+
+
+def get_sensor_range(hass: HomeAssistant, entity_id) -> tuple[float, float] | None:
+    """Retrieve the defined min and max range of a sensor.
+
+    Args:
+        hass: hass object to access states.
+        entity_id: sensor ID that range is from
+
+    Returns:
+        tuple{min, max} representing the range of the sensor
+
+    """
+    state = hass.states.get(entity_id)
+    if not state:
+        return None
+
+    min_value = state.attributes.get("min_value")
+    max_value = state.attributes.get("max_value")
+    if min_value is None or max_value is None:
+        return None
+    return min_value, max_value
--- homeassistant/components/lightmap/suggestion_helper.py
+++ homeassistant/components/lightmap/suggestion_helper.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 import statistics
 
 from homeassistant.core import HomeAssistant
-from homeassistant.components.lightmap.helpers import get_sensors
+from homeassistant.components.lightmap.helpers import get_sensors, get_sensor_range
 
 
 async def get_average_light_sensors(hass: HomeAssistant, hours=48) -> dict:
@@ -80,28 +80,6 @@ async def get_sensors_in_category_range(hass: HomeAssistant, category: str) -> d
                 within_category[entity_id] = average_value
 
     return within_category
-
-
-def get_sensor_range(hass: HomeAssistant, entity_id) -> tuple[float, float] | None:
-    """Retrieve the defined min and max range of a sensor.
-
-    Args:
-        hass: hass object to access states.
-        entity_id: sensor ID that range is from
-
-    Returns:
-        tuple{min, max} representing the range of the sensor
-
-    """
-    state = hass.states.get(entity_id)
-    if not state:
-        return None
-
-    min_value = state.attributes.get("min_value")
-    max_value = state.attributes.get("max_value")
-    if min_value is None or max_value is None:
-        return None
-    return min_value, max_value
 
 
 def is_number(value) -> bool:
